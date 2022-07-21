5113de7a0d9367b21c0088d51f605c33c95204f3
Harshvardhan Pandit <me@harshp.com>
2022-07-21 16:22:15 +0100

    Adds risk concepts related to levels, severity
    See changelog for more details.

--- documentation-generator/903_html.sh
+++ documentation-generator/903_html.sh
@@ -3,8 +3,19 @@
 
 #execute DPV HTML generation scripts
 
+CHECK_ERROR() {
+    retval=$?
+    if [ $retval -ne 0 ]; then
+        echo "Error"
+        echo $retval
+        exit 1;
+    fi
+}
+
 # Step3: generate HTML
 ./003_generate_respec_html.py
+CHECK_ERROR
 ./003_generate_respec_html_skos.py
+CHECK_ERROR
 ./003_generate_respec_html_owl.py
-
+CHECK_ERROR
